48a536f77ce26baa6ffab821c663485c9f9d3b1d
Victor Piolin <victorpiolin@MacBook-Pro-de-Victor.local>
2017-09-19 13:49:29 -0400

    Header clean App

--- App/App.js
+++ App/App.js
@@ -1,26 +1,55 @@
 import React, { Component } from 'react';
 import {
-  AppRegistry,
   StyleSheet,
   Text,
   View
 } from 'react-native';
+import {
+    Container,
+    Header,
+    Title,
+    Content,
+    Button,
+    Left,
+    Center,
+    Right,
+    Body,
+    Icon,
+    Drawer,
+    Card,
+    CardItem,
+} from 'native-base'
+
+import FontAwesome, { Icons } from 'react-native-fontawesome'
+
 
 export default class App extends Component {
     render() {
         return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    Bienvenue sur le TEST React-native
-                </Text>
-                <Text style={styles.instructions}>
-                    To get started, edit index.ios.js
-                </Text>
-                <Text style={styles.instructions}>
-                    Press Cmd+R to reload,{'\n'}
-                    Cmd+D or shake for dev menu
-                </Text>
-            </View>
+            <Container>
+                <Header>
+                    <Left style={styles.leftButton}>
+                        <Button transparent >
+                            <Icon ios='ios-menu' android="md-menu"/>
+                        </Button>
+                    </Left>
+                    <Body>
+                        <Title style={styles.titleApp}> Journal de Chantier de test </Title>
+                    </Body>
+                </Header>
+                <View style={styles.container}>
+                    <Text style={styles.welcome}>
+                        Bienvenue sur le TEST React-native
+                    </Text>
+                    <Text style={styles.instructions}>
+                        To get started, edit index.ios.js
+                    </Text>
+                    <Text style={styles.instructions}>
+                        Press Cmd+R to reload,{'\n'}
+                        Cmd+D or shake for dev menu
+                    </Text>
+                </View>
+            </Container>
         );
     }
 };
@@ -42,4 +71,11 @@ const styles = StyleSheet.create({
         color: '#333333',
         marginBottom: 5,
     },
+    titleApp: {
+        fontSize: 20,
+        fontWeight: '300',
+    },
+    leftButton: {
+        flex: 0
+    }
 });
